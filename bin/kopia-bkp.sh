1fa81c9430469b45878e2fc2b87b35026a557f1d
Tobias K <6317548+theCalcaholic@users.noreply.github.com>
2023-10-01 13:15:24 +0200

    BACKUPS/kopia.sh: Implement incremental backups using kopia
    Signed-off-by: Tobias K <6317548+theCalcaholic@users.noreply.github.com>

---
+++ bin/kopia-bkp.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+set -e
+
+create_data_snapshot() {
+  save_maintenance_mode
+
+  local MOUNTPOINT
+  MOUNTPOINT="$1"
+
+  btrfs-snp "${MOUNTPOINT}" manual 1 0 ../ncp-snapshots
+
+  restore_maintenance_mode
+}
+
+source /usr/local/etc/library.sh
+
+repo_type="$(source "${BINDIR}/BACKUPS/kopia.sh"; tmpl_repository_type)"
+repo_path="$(source "${BINDIR}/BACKUPS/kopia.sh"; tmpl_destination)"
+docker_args=()
+[[ "$repo_type" == "filesystem" ]] && docker_args=(-v "${repo_path}:/repository")
+data_dir="$(source "${BINDIR}/CONFIG/nc-data_dir.sh"; tmpl_data_dir)"
+cache_dir="${data_dir}/.kopia"
+mkdir -p "${cache_dir}"
+
+mountpoint="$( stat -c "%m" "$data_dir" )" || { echo "Error retrieving ncdata mountpoint"; return 1; }
+
+db_backup_dir="$(dirname "${data_dir}")"
+db_backup_file="ncp-db-$( date -Iseconds -u )-bkp.sql"
+mysqldump -u root --single-transaction nextcloud > "${db_backup_dir}/${db_backup_file}"
+
+cleanup(){
+  local ret=$?
+  rm -f "${db_backup_dir}/${db_backup_file}"
+  restore_maintenance_mode
+  exit $ret
+}
+trap cleanup EXIT
+
+docker run --rm \
+  -v /usr/local/etc/kopia:/app/config \
+  -v "${cache_dir}:/app/cache" \
+  -v "${db_backup_dir?}/${db_backup_file?}:/db/${db_backup_file}" \
+  "${docker_args[@]}" \
+  kopia/kopia:latest snapshot create "/db/${db_backup_file}"
+
+if [[ "$( stat -fc%T "$mountpoint" )" == "btrfs" ]]
+then
+  create_data_snapshot "${mountpoint}"
+  data_dir="$(dirname "${mountpoint}")/ncp-snapshots"
+else
+  save_maintenance_mode
+fi
+
+docker run --rm \
+  -v /usr/local/etc/kopia:/app/config \
+  -v "${cache_dir}:/app/cache" \
+  -v "${data_dir?}:/ncdata" \
+  "${docker_args[@]}" \
+  kopia/kopia:latest snapshot create /ncdata
+
---
+++ bin/ncp/BACKUPS/kopia.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+# Nextcloud backups
+#
+# Copyleft 2023 by Tobias Knöppler
+# GPL licensed (see end of file) * Use at your own risk!
+#
+
+tmpl_destination() {
+  find_app_param kopia DESTINATION
+}
+
+tmpl_repository_type() {
+  [[ "$DESTINATION" =~ .*'@'.*':'.* ]] && echo "sftp" || echo "filesystem"
+}
+
+install() {
+ :;
+}
+
+configure() {
+  mkdir -p /usr/local/etc/kopia
+  mkdir -p /var/log/kopia
+  hostname="$(ncc config:system:get overwrite.cli.url)"
+
+  docker_args=()
+  kopia_args=()
+  if [[ "$DESTINATION" =~ .*'@'.*':'.* ]]
+  then
+    repo_type="sftp"
+    sftp_user="${DESTINATION%@*}"
+    sftp_host="${DESTINATION#*@}"
+    sftp_host="${sftp_host%:*}"
+    repo_path="${DESTINATION#*:}"
+    ssh -o "BatchMode=yes" "${sftp_user}@${sftp_host}" || { echo "SSH non-interactive not properly configured"; return 1; }
+    kopia_args=(--host "${sftp_host}" --user "${sftp_user}" --path "${repo_path}")
+  else
+    repo_type="filesystem"
+    repo_path="${DESTINATION}"
+    docker_args=(-v "${repo_path}:/repository")
+    kopia_args=(--path "/repository")
+  fi
+
+
+  echo "Attempting to connect to existing repository first..."
+  docker run --rm --pull always \
+    -v /usr/local/etc/kopia:/app/config \
+    -v /var/log/kopia:/app/logs \
+    "${docker_args[@]}" \
+    kopia/kopia:latest repository connect "${repo_type}" \
+      "${kopia_args[@]}" \
+      --override-username ncp \
+      --override-hostname "$hostname" || {
+    echo "Creating new repository..."
+    docker run --rm --pull always \
+      -v /usr/local/etc/kopia:/app/config \
+      -v /var/log/kopia:/app/logs \
+      "${docker_args[@]}" \
+      kopia/kopia:latest repository create "${repo_type}" \
+        "${kopia_args[@]}" \
+        --override-username ncp \
+        --override-hostname "$hostname"
+  }
+
+  docker run --rm --pull always \
+    -v /usr/local/etc/kopia:/app/config \
+    -v /var/log/kopia:/app/logs \
+    -v "${DESTINATION}:/repository" \
+    kopia/kopia:latest policy set --global \
+      --keep-annual 2 --keep-monthly 12 --keep-weekly 4 --keep-daily 7 --keep-hourly 24 \
+      --add-ignore '/ncdata/.opcache' \
+      --add-ignore '/ncdata/nextcloud.log' \
+      --add-ignore '/ncdata/ncp-update-backups' \
+      --add-ignore '/ncdata/appdata_*/preview/*' \
+      --add-ignore '/ncdata/*/cache' \
+      --add-ignore '/ncdata/*/uploads' \
+      --add-ignore '/ncdata/.data_*'
+
+  cat > /etc/cron.hourly/ncp-kopia <<EOF
+#!/bin/bash
+/usr/local/bin/kopia-bkp.sh
+EOF
+  echo "Repository initialized successfully"
+
+}
